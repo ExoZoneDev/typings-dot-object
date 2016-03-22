0ec43e443a84a6b47393d788c3193b3a4aa02083
James Birtles <unwrittenfun@gmail.com>
2016-03-22 18:49:21 +0000

    Create dot-object.d.ts

---
+++ dot-object.d.ts
@@ -0,0 +1,11 @@
+export function move(from: string, to: string, obj: any): void;
+export function copy(from: string, to: string, fromObj: any, toObj: any): void;
+export function transfer(from: string, to: string, fromObj: any, toObj: any): void;
+export function str(path: string, value: any, target: any): any;
+export function pick(path: string, obj: any, remove?: boolean): any;
+export function remove(path: string, obj: any): any;
+export function del(path: string, obj: any): any;
+export function transform(recipe: any, source: any, target?: any): any | void;
+
+export function object(obj: any): any;
+export function dot(obj: any, target?: any): any;
